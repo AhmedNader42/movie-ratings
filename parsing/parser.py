419af7935770ae1914b2210d1e759b21764c1ff3
Ahmed Nader <ahmednader111@gmail.com>
2024-04-18 04:14:37 +0300

    Extracted year from data using regex

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
 
 spark = (
     SparkSession.builder.master("local[*]")
@@ -7,5 +9,6 @@ spark = (
 )
 
 movies = spark.read.option("header", True).csv("data/ml-latest-small/movies.csv")
-
-movies.show(5)
+movies_year = movies.withColumn("year", F.regexp_extract("title", "(\d{4})", 1))
+movies_grouped_By_Year = movies_year.groupBy("year").count().orderBy("year")
+movies_grouped_By_Year.show()
