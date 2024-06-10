0a6d3946fd55362bbc06b56314684794874f4865
Ahmed Nader <ahmednader111@gmail.com>
2024-06-11 02:09:37 +0300

    - Adding the genre split code. - TODO: add the genres as a column

--- parsing/parser.py
+++ parsing/parser.py
@@ -9,6 +9,15 @@ spark = (
 )
 
 movies = spark.read.option("header", True).csv("data/ml-latest-small/movies.csv")
-movies_year = movies.withColumn("year", F.regexp_extract("title", "(\d{4})", 1))
-movies_grouped_By_Year = movies_year.groupBy("year").count().orderBy("year")
-movies_grouped_By_Year.show()
+# Extract the year from the movie title
+movies_with_year = movies.withColumn("year", F.regexp_extract("title", "(\d{4})", 1))
+
+# Extract the genre tags from genres column
+movies_genre_separated = movies_with_year.rdd.flatMap(lambda x: x.genres.split("|"))
+
+
+print(movies_genre_separated.collect())
+
+
+# movies_grouped_By_Year = movies_with_year.groupBy("year").count().orderBy("year")
+# movies_grouped_By_Year.show()
